7f2a5906fd87ecfd95217435da742e08b0bce571
Deeksha Ganesh <dg575265@gmail.com>
2026-09-11 21:43:24 +0530

    ci: add GitHub Pages deployment workflow for NaanStop

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import StoreContextProvider from './context/StoreContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
         <StoreContextProvider>
             <App />
         </StoreContextProvider>
